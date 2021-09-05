19f8caf40bd3a3a8805de3dbbe15ad37367ecced
dmbsseller <DMBS_el@mail.ru>
2021-09-05 17:02:53 +0300

    new example of 'Adapter' pattern

---
+++ Structural/Adapter/Adapter - simple example/Adapter/Adapter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Target target = new Adapter();
+            target.Request();
+            Console.ReadLine();
+        }
+    }
+
+    class Target
+    {
+        public virtual void Request()
+        {
+            Console.WriteLine("Called Target Request");
+        }
+    }
+
+    class Adapter : Target
+    {
+        private Adaptee _adaptee = new Adaptee();
+        public override void Request()
+        {
+            _adaptee.SpecificRequest();
+        }
+    }
+
+    class Adaptee
+    {
+        public void SpecificRequest()
+        {
+            Console.WriteLine("Called Specific Request");
+        }
+    }
+}
